5404f51524ce48bd6590516baa0fae5711cf43e3
siggu <m0_w_0m@naver.com>
2025-04-12 20:50:31 +0900

    feat: 볼륨 변경

--- utils/play-sound.ts
+++ utils/play-sound.ts
@@ -1,5 +1,5 @@
 export const playSound = (type: 'success' | 'fail' | 'reset') => {
   const audio = new Audio(`/sounds/${type}.mp3`);
-  audio.volume = 0.2;
+  audio.volume = 0.1;
   audio.play();
 };
