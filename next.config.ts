67432b762f2a70c5ec6258434df145dc6b198d45
siggu <m0_w_0m@naver.com>
2025-04-14 21:43:10 +0900

    feat: pwa 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const withPWA = require('next-pwa')({
   dest: 'public',
+  disable: isDev,
   register: true,
   skipWaiting: true,
 });
-
 
 export default withPWA(nextConfig);
